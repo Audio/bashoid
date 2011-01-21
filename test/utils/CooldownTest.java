c97863ddec9efdd86946c74585b685be773c19c7
Audio <horkej@hotmail.com>
2011-01-21 16:07:03 +0100

    Preparation for unit testing.

---
+++ test/utils/CooldownTest.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class CooldownTest {
+
+    @Test
+    public void testEverything() {
+        assertFalse(false);
+    }
+
+}
