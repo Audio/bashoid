a4ad70f2c7f9ce773995599a39e5b3d10ed5e21c
Audio <horkej@hotmail.com>
2012-01-16 18:28:24 +0100

    Improved Translation addon: display more translations. Added support to handle multiword input.

--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -1,22 +1,31 @@
 package translator;
 
+import org.jsoup.Jsoup;
 import bashoid.Message;
 import bashoid.Addon;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.ParseException;
+import java.util.ArrayList;
 import utils.WebPage;
+import utils.XMLParser;
 
 import static utils.Constants.*;
 
 
 public class Translator extends Addon {
 
-    private static final String langs[] =
+    private static final String LANGS[] =
     {
         "cz", "en", "ru", "de", "fr", "it", "es", "sk"
     };
-    private static final byte langsCount = 8;
+    private static final byte LANGS_COUNT = (byte) LANGS.length;
+    private static final String NO_TRANSLATION = "No translation found";
+    private static final byte MAX_TRANSLATION_COUNT = 5;
 
 
-    private String getAddress(String query, String langFrom, String langTo) {
+    private String getAddress(String query, String langFrom, String langTo) throws UnsupportedEncodingException {
+        query = URLEncoder.encode(query, "UTF-8");
         return "http://slovnik.seznam.cz/" + langFrom + "-" + langTo + "/?q=" + query;
     }
 
@@ -29,61 +38,48 @@ public class Translator extends Addon {
         String content = loadPage(address);
 
         if(content.indexOf("nebylo nic nalezeno") != NOT_FOUND || content.indexOf("li jste hledat?") != NOT_FOUND)
-            return "No translation found";
+            return NO_TRANSLATION;
 
-        String toSearch = "<div id=\"fastMeanings\">";
-        int pos = content.indexOf(toSearch);
-        if (pos == NOT_FOUND)
-            throw new Exception("Cannot... nothing!");
+        String[] translations = getAllPossibleTranslations(content);
+        String output = "";
+        if (translations.length > 0) {
 
+            for (byte i = 0; i < translations.length && i < MAX_TRANSLATION_COUNT; ++i)
+                output += translations[i] + ", ";
+            output = output.substring(0, output.length() - 2);
 
-        int begin = pos + toSearch.length();
-        int end = content.indexOf("<br", begin);
-        if (end == NOT_FOUND)
-            end = content.indexOf("</div>", begin);
+            if (translations.length > MAX_TRANSLATION_COUNT)
+                output += " | " + address;
 
-        String sub = content.substring(begin, end);
-        String result = "";
+            return output;
 
-        if(sub.indexOf("<span class=\"w\">") != NOT_FOUND && sub.indexOf("<a href") == NOT_FOUND)
-            return "No translation found, it is probably the same in target language";
+        } else {
+            return NO_TRANSLATION;
+        }
+    }
 
-        toSearch = "\">";
-        pos = sub.indexOf("<a href");
-        int comma = 0;
-        while(pos != NOT_FOUND)
-        {
-            pos = sub.indexOf(toSearch, pos);
+    private String[] getAllPossibleTranslations(String content) throws ParseException {
+        content = XMLParser.getSnippet(content, "<div id=\"fastMeanings\">", "</div>");
+        String[] translations = content.split("<br />");
 
-            begin = pos + toSearch.length();
-            end = sub.indexOf("</a>", begin);
-            if (end == NOT_FOUND)
-            {
-                result = sub;
-                break;
-            }
-            result += sub.substring(begin, end);
-            pos = sub.indexOf("<a href", end);
-            if(comma != NOT_FOUND)
-            {
-                comma = sub.indexOf("<span class=\"comma\">", end);
-                if(comma != NOT_FOUND && comma < pos)
-                    result += ",";
-            }
-            result += " ";
+        ArrayList<String> cleanedTranslations = new ArrayList<String>();
+        for (String trans : translations) {
+            trans = Jsoup.parse(trans).text();
+            trans.replaceFirst(" ,", ",");
+            if ( trans.length() > 0 )
+                cleanedTranslations.add(trans);
         }
 
-        return result + "| " + address;
+        return (String[]) cleanedTranslations.toArray(new String[0]);
     }
 
     private String getLang(String message, boolean from) {
         return from ? message.substring(0, 2) : message.substring(6, 8);
     }
 
-    public boolean isLangAllowed(String lang)
-    {
-        for(byte i = 0; i < langsCount; ++i)
-            if(lang.equals(langs[i]))
+    public boolean isLangAllowed(String lang) {
+        for(byte i = 0; i < LANGS_COUNT; ++i)
+            if(lang.equals(LANGS[i]))
                 return true;
         return false;
     }
@@ -100,10 +96,11 @@ public class Translator extends Addon {
         try {
             String langFrom = getLang(message.text, true);
             String langTo = getLang(message.text, false);
-            if(isLangAllowed(langFrom) && isLangAllowed(langTo))
-            {
+            if (isLangAllowed(langFrom) && isLangAllowed(langTo)) {
                 String response = getTranslation(getAddress(message.text.substring(9), langFrom, langTo));
                 reaction.add(response);
+            } else {
+                reaction.add(NO_TRANSLATION);
             }
         } catch (Exception e) {
             setError(e);
