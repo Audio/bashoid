abf0fe6aeab6506a2e614bd83a91674852af0bae
Audio <horkej@hotmail.com>
2012-01-18 17:12:30 +0100

    Simplify removeHTML method in da Formatter klas.

--- src/utils/Formatter.java
+++ src/utils/Formatter.java
@@ -1,64 +1,12 @@
 package utils;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static utils.Constants.*;
+import org.jsoup.Jsoup;
 
 
 public class Formatter {
 
-    private static HashMap<String, String> tagList;
-
-    static {
-        tagList = new HashMap<String, String>();
-        tagList.put("&lt;",   "<");
-        tagList.put("&gt;",   ">");
-        tagList.put("&quot;", "\"");
-        tagList.put("&apos;", "'");
-        tagList.put("&nbsp;", " ");
-        tagList.put("&amp;",  "&");
-        tagList.put("<br />",  "");
-        tagList.put("<span style=\"color: ;\">", "");
-        tagList.put("</span>", "");
-    }
-    
     public static String removeHTML(String htmlToPlain) {
-        for ( Map.Entry<String, String> entry : tagList.entrySet() )
-            htmlToPlain = htmlToPlain.replace( entry.getKey(), entry.getValue() );
-
-        int index = 0;
-        int indexEnd = 0;
-
-        while(true) {
-            boolean found = false;
-            while(!found) {
-                index = htmlToPlain.indexOf("&#", indexEnd);
-                if(index == NOT_FOUND)
-                    break;
-
-                indexEnd = htmlToPlain.indexOf(";", index);
-
-                if(indexEnd == NOT_FOUND)
-                    break;
-
-                if(indexEnd - index > 5)
-                    continue;
-
-                found = true;
-            }
-            if(!found)
-                break;
-
-            try{
-                char ascii = (char)Integer.valueOf(htmlToPlain.substring(index+2, indexEnd)).byteValue();
-                htmlToPlain = htmlToPlain.replaceAll(htmlToPlain.substring(index, indexEnd+1), Character.toString(ascii));
-            }
-            catch(NumberFormatException e){
-            }
-        }
-
-        return htmlToPlain;
+        return Jsoup.parse(htmlToPlain).text();
     }
 
 }
