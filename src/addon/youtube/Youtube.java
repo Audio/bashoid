937ea88dbf11d176cbb4b22e9420b32a4a6624c8
Audio <horkej@hotmail.com>
2012-12-21 18:29:55 +0100

    YouTube addon: short link can end with the question mark

--- src/addon/youtube/Youtube.java
+++ src/addon/youtube/Youtube.java
@@ -13,7 +13,7 @@ import utils.*;
 public class Youtube extends Addon {
 
     private static final Pattern LONG_URL = Pattern.compile("youtube\\.com.*v=([^&$]{11})(&|#| |$)");
-    private static final Pattern SHORT_URL = Pattern.compile("youtu\\.be/([^&\\?$]{11})(&|#| |$)");
+    private static final Pattern SHORT_URL = Pattern.compile("youtu\\.be/([^&\\?$]{11})(\\?|&|#| |$)");
     private ArrayList<LinkInfo> cache = new ArrayList<>();
 
 
